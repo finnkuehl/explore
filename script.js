38b2768c64c8cfb735bd2149331cada8a6c25716
finnkuehl <131387701+finnkuehl@users.noreply.github.com>
2023-04-21 23:38:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2707
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+// Initialisierung der Karte
+function initMap() {
+  // Koordinaten des Zentrums der Karte
+  const center = { lat: 52.520008, lng: 13.404954 };
 
-\f0\fs24 \cf0 function markInterested() \{\
-	// Hier k\'f6nnen Sie den Code schreiben, der den Ort als interessant markiert\
-	// und auf der Karte darstellt\
-	// Zum Beispiel:\
-	console.log("Ort als interessant markiert");\
-\}\
-\
-function markNotInterested() \{\
-	// Hier k\'f6nnen Sie den Code schreiben, der den Ort als nicht interessant markiert\
-	// und auf der Karte ausblendet\
-	// Zum Beispiel:\
-	console.log("Ort als nicht interessant markiert");\
-\}\
-}
+  // Optionen für die Karte
+  const options = {
+    zoom: 10,
+   
